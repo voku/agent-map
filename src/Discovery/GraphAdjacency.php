35ef44344c47017fed4e2e507aba024bdd2f1dd5
Lars Moelleken <lars@moelleken.org>
2026-09-06 09:26:34 +0200

    refactor(graph): delegate adjacency to agent-graph

--- src/Discovery/GraphAdjacency.php
+++ src/Discovery/GraphAdjacency.php
@@ -4,41 +4,58 @@ declare(strict_types=1);
 
 namespace voku\AgentMap\Discovery;
 
+use RuntimeException;
+use voku\AgentGraph\Graph\GraphAdjacency as SharedGraphAdjacency;
+use voku\AgentGraph\Graph\GraphRelation;
 use voku\AgentMap\Index\AgentMapIndex;
+use voku\AgentMap\Index\GraphProjectionFactory;
 use voku\AgentMap\Index\RelationEntry;
 
 final readonly class GraphAdjacency
 {
-    /** @var array<string, list<RelationEntry>> */
-    private array $incoming;
+    private SharedGraphAdjacency $adjacency;
 
-    /** @var array<string, list<RelationEntry>> */
-    private array $outgoing;
+    /** @var array<string, RelationEntry> */
+    private array $relationsById;
 
     public function __construct(AgentMapIndex $map)
     {
-        $incoming = [];
-        $outgoing = [];
+        $relationsById = [];
         foreach ($map->relations as $relation) {
-            $outgoing[$relation->sourceId][] = $relation;
-            foreach ($relation->targetIds as $targetId) {
-                $incoming[$targetId][] = $relation;
-            }
+            $relationsById[$relation->id] = $relation;
         }
 
-        $this->incoming = $incoming;
-        $this->outgoing = $outgoing;
+        $this->relationsById = $relationsById;
+        $this->adjacency = new SharedGraphAdjacency((new GraphProjectionFactory())->fromIndex($map));
     }
 
     /** @return list<RelationEntry> */
     public function incoming(string $nodeId): array
     {
-        return $this->incoming[$nodeId] ?? [];
+        return $this->ownerRelations($this->adjacency->incoming($nodeId));
     }
 
     /** @return list<RelationEntry> */
     public function outgoing(string $nodeId): array
     {
-        return $this->outgoing[$nodeId] ?? [];
+        return $this->ownerRelations($this->adjacency->outgoing($nodeId));
+    }
+
+    /**
+     * @param list<GraphRelation> $relations
+     * @return list<RelationEntry>
+     */
+    private function ownerRelations(array $relations): array
+    {
+        $ownerRelations = [];
+        foreach ($relations as $relation) {
+            $owner = $this->relationsById[$relation->id] ?? null;
+            if ($owner === null) {
+                throw new RuntimeException('Projected graph relation is missing from agent-map owner state: ' . $relation->id);
+            }
+            $ownerRelations[] = $owner;
+        }
+
+        return $ownerRelations;
     }
 }
