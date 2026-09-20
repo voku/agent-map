d6b7d211e4c6dd2312a9601954a595ba3bfb6741
Lars Moelleken <lars@moelleken.org>
2026-09-20 09:59:55 +0200

    fix(search): route metadata repair through refresh

--- src/Search/SearchReadinessInspector.php
+++ src/Search/SearchReadinessInspector.php
@@ -95,6 +95,7 @@ final readonly class SearchReadinessInspector
                 searchSnapshot: null,
                 reason: 'search_snapshot_missing',
                 message: 'Search index does not record the Map snapshot it was built from.',
+                recoveryCommand: $this->recoveryCommand('refresh', $index, $indexPath, $databasePath),
             );
         }
 
